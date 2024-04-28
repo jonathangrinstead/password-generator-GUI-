2f2287930dd5696ab144888a51e3f7493be42ec6
jonathangrinstead <jonathangrinstead.design@gmail.com>
2024-04-28 12:41:49 +0100

    General Layout to application complete

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import PySimpleGUI as sg
 
+sg.theme('BluePurple')
+
 reply = ''
 
 # All the stuff inside your window.
-layout = [  [sg.Text("What's your name?")],
-            [sg.InputText()],
-            [sg.Text(reply, key='reply')],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+layout = [[ sg.Push(),sg.Text('Generate a Password'), sg.Push()],
+            [ sg.Push(), sg.Button('Generate'), sg.Push()] ]
 
 # Create the Window
-window = sg.Window('Desktop.GPT', layout)
+window = sg.Window('Password Generator', layout,size=(300,150 ))
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -19,15 +19,11 @@ while True:
 
     print(name)
 
-    reply = f"Hello {name} !"
-
     if event == 'Ok':
         window['reply'].update(reply)
 
     # if user closes window or clicks cancel
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
-
-    print('Hello', values[0], '!')
 
 window.close()
