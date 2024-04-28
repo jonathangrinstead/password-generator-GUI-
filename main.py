14bc25ef7e3008d109bf868f9eabb3014b289b96
jonathangrinstead <jonathangrinstead.design@gmail.com>
2024-04-28 16:17:27 +0100

    added ability to copy password to clipboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import string
 import secrets
+import pyperclip
 
 sg.theme('BluePurple')
 
@@ -9,10 +10,11 @@ def password_generator(length=12):
     password = ''.join(secrets.choice(alphabet) for i in range(length))
     return password
 
-# All the stuff window.
+# All the stuff in the window.
 layout = [[ sg.Push(),sg.Text('Generate a Password'), sg.Push()],
             [ sg.Push(), sg.Button('Generate', key='Generate'), sg.Push()],
-            [sg.Push(),sg.Text('', size=(30, 1), key='-OUTPUT-'),sg.Push()]]
+            [sg.Push(),sg.Push(),sg.Text('', size=(30, 1), key='-OUTPUT-'),sg.Push()], 
+            [sg.Push(),sg.Button('Copy to Clipboard', key='Copy'),sg.Push()]]
 
 # Create the Window
 window = sg.Window('Password Generator', layout,size=(300,150 ))
@@ -25,6 +27,12 @@ while True:
         # Call the password generator function and update the output Text Element
         new_password = password_generator()
         window['-OUTPUT-'].update(new_password)
+
+    elif event == 'Copy':
+        # Copy the currently displayed password to the clipboard
+        current_password = window['-OUTPUT-'].get()
+        pyperclip.copy(current_password)
+        sg.popup('Password copied to clipboard!')
 
     # if user closes window or clicks cancel
     if event == sg.WIN_CLOSED or event == 'Cancel':
